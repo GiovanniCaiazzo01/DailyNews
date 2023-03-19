e9a51c674a720a4b5b806584e330f11297736ebb
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2023-03-19 18:43:09 +0100

    chore: removed db Name from dataBase

--- portal-BEN/config/config.js
+++ portal-BEN/config/config.js
@@ -2,5 +2,5 @@ require("dotenv").config({ path: "./.env" });
 module.exports = {
   PORT: process.env.PORT || 3000,
   DB_URI: process.env.DB_URI,
-  DB_NAME: process.env.DB_NAME || "Gino",
+  DB_NAME: process.env.DB_NAME,
 };
