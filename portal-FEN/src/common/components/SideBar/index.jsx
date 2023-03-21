f3401abe5a75487749434a3a09eb20d3c08b0157
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2023-03-21 11:33:39 +0100

    feat: updated Logo

--- portal-FEN/src/common/components/SideBar/index.jsx
+++ portal-FEN/src/common/components/SideBar/index.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
-import useLogout from "../../../hooks/useLogout";
 import useUser from "../../../hooks/useUser";
 import "./style.css";
 
@@ -29,10 +28,7 @@ const SideBar = () => {
       </div>
 
       <div className="logo">
-        <img
-          src="https://png.pngtree.com/template/20191017/ourlarge/pngtree-letter-n-logo-vector-designs-initials-n-logo-image_320358.jpg"
-          alt="..."
-        />
+        <img src="../../../../public/logo.svg" alt="..." />
 
         <h3>{user?.name}</h3>
       </div>
