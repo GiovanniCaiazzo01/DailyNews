9239b87d3e021ce124263dc73bb39b94341dcd87
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2023-03-25 12:36:31 +0100

    refactor: added error messages on newsController

--- portal-BEN/components/news/newsController.js
+++ portal-BEN/components/news/newsController.js
@@ -1,5 +1,6 @@
 const { KEY_NEWSDATA } = require("./config/config");
 const { default: axios } = require("axios");
+const { ERRORS } = require("../../utils/utils");
 
 module.exports = {
   list: async () => {
@@ -18,7 +19,7 @@ module.exports = {
         }
       );
       if (news.status !== 200) {
-        throw new Error("It was not possible to retrieve the latest news");
+        throw new Error(ERRORS.UNRECOVERABLE_NEWS);
       }
 
       const to_return = news.data.results.map((n) => {
@@ -43,7 +44,7 @@ module.exports = {
   },
   filtered_list: async ({ language }) => {
     if (!language) {
-      return { result: false, message: "Error occurred" };
+      return { result: false, message: ERRORS.MISSING_LANGUAGE };
     }
 
     const languages = {
@@ -76,7 +77,7 @@ module.exports = {
         }
       );
       if (news.status !== 200) {
-        throw new Error("It was not possible to retrieve the latest news");
+        throw new Error(ERRORS.UNRECOVERABLE_NEWS);
       }
 
       const to_return = news.data.results.map((n) => {
--- portal-BEN/utils/utils.js
+++ portal-BEN/utils/utils.js
@@ -3,16 +3,19 @@ const jwt = require("jsonwebtoken");
 module.exports = {
   ERRORS: {
     MISSING_FIELD: (field) => {
-      return `Please fill the ${field} field `;
+      return `Please fill the ${field} field`;
     },
     GENERIC:
       "Something went wrong with an operation that you are trying to perform 👀, try it again or try later ",
     EMAIL_OR_PASSWORD: "Email or password are incorrect",
+
+    UNRECOVERABLE_NEWS: "It was not possible to retrieve the latest news",
+    MISSING_LANGUAGE: "Please fill the language field ",
   },
 
   JWT_ERRORS: {
-    TokenExpiredError,
-    JsonWebTokenError,
+    TokenExpiredError: "TokenExpiredError",
+    JsonWebTokenError: "JsonWebTokenError",
   },
   createToken: (name, age, email) => {
     const payload = {
