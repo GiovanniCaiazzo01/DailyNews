bb75bc41f9a6b4c21685730a810b4a6e5cd37a1c
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2023-03-30 12:26:44 +0200

    refactor: now showing different bar based on width

--- portal-FEN/src/common/components/PublicRoute/index.jsx
+++ portal-FEN/src/common/components/PublicRoute/index.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import loadable from "@loadable/component";
-
+import useWindowDimension from "../../../hooks/getWindowDimension";
 const SideBar = loadable(() => import("../"), {
   resolveComponent: (components) => components.SideBar,
 });
 
+const BottomBar = loadable(() => import("../"), {
+  resolveComponent: (components) => components.BottomBar,
+});
+
 const PublicRoute = ({ component: Component }) => {
+  const { height, width } = useWindowDimension();
+
+  console.log("height", height, "width", width);
+
   return (
     <>
-      <SideBar />
+      {width <= 540 ? <BottomBar /> : <SideBar />}
       <div
         style={{
           marginTop: "50px",
