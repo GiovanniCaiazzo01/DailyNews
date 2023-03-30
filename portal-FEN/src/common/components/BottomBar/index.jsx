d90a7d3bb61450ceb952f2c0879899029e1f4e49
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2023-03-30 12:26:14 +0200

    wip: BottomBar

---
+++ portal-FEN/src/common/components/BottomBar/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+const BottomBar = () => {
+  return (
+    <div id="bottom-bar-container">
+      <div id="bottom-bar-item">Logout</div>
+      <div id="bottom-bar-item">News</div>
+      <div id="bottom-bar-item">Saved</div>
+      <div id="bottom-bar-item">Profile</div>
+      <div id="bottom-bar-item">Help</div>
+    </div>
+  );
+};
+
+export { BottomBar };
