e8c2b58da6cff49ba15d5e44bdb3db5763680398
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2023-03-31 20:39:14 +0200

    build

--- portal-FEN/dist/assets/useLogout-28987db4.js
+++
@@ -1,1 +0,0 @@
-import{u as s,c as u,b as r,r as c}from"./index-870b2980.js";const f=()=>{const{verify_auth:t}=s(),{pathname:o}=u(),a=r(),e=()=>(localStorage.clear(),t());c.useEffect(()=>{e(),o==="/logout"&&a("/")},[])};export{f as default};
