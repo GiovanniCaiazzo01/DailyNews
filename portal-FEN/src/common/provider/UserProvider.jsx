fad45033a9e993f1d1fc3ae442ed6a2d078b2372
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2023-04-09 19:58:48 +0200

    refactor: improved userProvider performance

--- portal-FEN/src/common/provider/UserProvider.jsx
+++ portal-FEN/src/common/provider/UserProvider.jsx
@@ -1,26 +1,32 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useCallback, useEffect, useState } from "react";
 import { HTTPClient } from "../../api/HTTPClients";
 import useAuth from "../../hooks/useAuth";
 import jwtDecode from "jwt-decode";
 const UserContext = createContext();
 
+const fetch_user = async () => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    const { email } = await jwtDecode(token);
+    const response = await HTTPClient.get(`/users`, email);
+    const user = response.data;
+    return user;
+  }
+};
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState();
   const { isLogged } = useAuth();
 
-  const fetch_user = async () => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      const { email } = await jwtDecode(token);
-      const response = await HTTPClient.get(`/users`, email);
-      const user = response.data;
-      setUser(() => user);
-    }
-  };
+  const memorizedFetchUser = useCallback(
+    async () => await fetch_user(),
+    [isLogged]
+  );
 
   useEffect(() => {
-    fetch_user();
-  }, [isLogged]);
+    if (isLogged) {
+      memorizedFetchUser().then((user) => setUser(() => user));
+    }
+  }, [memorizedFetchUser]);
 
   return (
     <UserContext.Provider value={{ user, fetch_user }}>
